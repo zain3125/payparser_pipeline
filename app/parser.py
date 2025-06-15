1c35cddcf48a41a2ccd0bad8fabe52c436aa9963
zain <zeine712342@gmail.com>
2025-06-15 08:44:47 +0300

    Improve extracting date for cash

--- app/parser.py
+++ app/parser.py
@@ -18,7 +18,7 @@ def parse_transaction_details_cash(text, filename):
     text = text.translate(str.maketrans("٠١٢٣٤٥٦٧٨٩", "0123456789"))
     amount = extract_amount(text, r"([\d,]+)\s*جنيه")
     phone_number = extract_egyptian_phone_number(text)
-    date = extract_date(text, r"(\d{1,2})\s(\w+)\s(\d{1,2}:\d{2})\s?(\d{4})", is_arabic=True)
+    date = extract_date(text, r"(\d{1,2})\.?\s+(\w+)\s+(\d{1,2}:\d{2})(\d{4})", is_arabic=True)
     date = format_date_for_sqlite(date)
     sender = os.path.basename(os.path.dirname(filename))
     match = re.search(r"\b(\d{12})\b", text)
