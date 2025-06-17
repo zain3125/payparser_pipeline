bb1dbf21080a1b3fbe1384524725e6dba0cf0d3d
zain <zeine712342@gmail.com>
2025-06-17 09:20:01 +0300

    save transactions to csv file every day and archive transactions by day

--- app/save.py
+++ app/save.py
@@ -1,32 +1,40 @@
+from datetime import datetime
 import os
 import sqlite3
 import pandas as pd
-from tkinter import ttk, messagebox, tk
 from app.app_config import DB_NAME, SAVEING_PATH
 
-def export_to_excel():
+LAST_ID_FILE = os.path.join(SAVEING_PATH, 'last_id.txt')
+
+def export_to_csv():
     try:
-        conn = sqlite3.connect(DB_NAME)
-        df = pd.read_sql_query("SELECT * FROM transactions", conn)
-        conn.close()
-
-        file_name = f"Transactions data.xlsx"
-
         os.makedirs(SAVEING_PATH, exist_ok=True)
 
+        if os.path.exists(LAST_ID_FILE):
+            with open(LAST_ID_FILE, 'r') as f:
+                last_id = int(f.read().strip() or 0)
+        else:
+            last_id = 0
+
+        conn = sqlite3.connect(DB_NAME)
+
+        query = f"SELECT * FROM transactions WHERE id > {last_id}"
+        df = pd.read_sql_query(query, conn)
+        conn.close()
+
+        if df.empty:
+            print("No new records found.")
+            return
+
+        file_name = f"Transactions data {datetime.now().strftime('%d %B %Y')}.csv"
         file_path = os.path.join(SAVEING_PATH, file_name)
 
-        df.to_excel(file_path, index=False)
-        messagebox.showinfo("Saved", f"Saved correctly in\n{file_path}")
+        df.to_csv(file_path, index=False)
+        print("Success", f"Data saved to {file_path}")
+
+        new_last_id = df['id'].max()
+        with open(LAST_ID_FILE, 'w') as f:
+            f.write(str(new_last_id))
+
     except Exception as e:
-        messagebox.showerror("Error", f"Failed to save:\n{e}")
-
-# GUI setup
-root = tk.Tk()
-root.title("Export to Excel")
-root.geometry("300x150")
-
-btn_export = ttk.Button(root, text="Export to Excel Sheet", command=export_to_excel)
-btn_export.pack(pady=50)
-
-root.mainloop()
+        print("Error", f"Failed to save:\n{e}")
--- app/tasks/rename.py
+++ app/tasks/rename.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 import os
-from app.tasks.airflow_config import WATCH_FOLDER, PROCESSED_FILE
+from app.tasks.airflow_config import WATCH_FOLDER
 from app.parser import parse_transaction_details_cash
 from app.utils import extract_transaction_id
 
@@ -42,13 +43,24 @@ def rename_images_by_transaction_id(ti):
                         os.rename(src, dst)
                         print(f"Renamed: {fname} -> {txid}{ext}")
 
-                        # Append to processed file
-                        with open(PROCESSED_FILE, 'a') as f:
-                            f.write(os.path.basename(dst) + '\n')
-
                     except Exception as e:
                         print(f"Error renaming file: {e}")
                     break
 
             if not found:
                 print(f"Source file not found: {fname}")
+
+    # Move processed files to archive
+    archive_folder = os.path.join(os.path.dirname(WATCH_FOLDER), f"Transactions {datetime.now().strftime('%d %B %Y')}")
+    os.makedirs(archive_folder, exist_ok=True)
+
+    for root, dirs, files in os.walk(WATCH_FOLDER):
+        for filename in files:
+            src = os.path.join(root, filename)
+            dst = os.path.join(archive_folder, filename)
+
+            try:
+                os.rename(src, dst)
+                print(f"Moved to archive: {filename}")
+            except Exception as e:
+                print(f"Error moving file {filename} to archive: {e}")
